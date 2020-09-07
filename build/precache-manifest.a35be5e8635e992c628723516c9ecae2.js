40bfaed742f82f5f3984f32ba3126a44a3e50e55
Mihir <mihir0699@gmail.com>
2020-09-07 15:02:41 +0530

    d

---
+++ build/precache-manifest.a35be5e8635e992c628723516c9ecae2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56b1eb9622026ac15a3bcea66e5bb668",
+    "url": "/react123/index.html"
+  },
+  {
+    "revision": "7755fd239a9c4db2558f",
+    "url": "/react123/static/css/2.02c997e6.chunk.css"
+  },
+  {
+    "revision": "7de7f37d35ee3932e50f",
+    "url": "/react123/static/css/main.6da1cfdb.chunk.css"
+  },
+  {
+    "revision": "7755fd239a9c4db2558f",
+    "url": "/react123/static/js/2.7d4dfd30.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react123/static/js/2.7d4dfd30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7de7f37d35ee3932e50f",
+    "url": "/react123/static/js/main.b3a1fad3.chunk.js"
+  },
+  {
+    "revision": "7ce8d338184d237ee099",
+    "url": "/react123/static/js/runtime-main.cfe13659.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react123/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react123/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react123/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react123/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react123/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
